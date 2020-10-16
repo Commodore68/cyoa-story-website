6c38f9c4d1c7ec908e0e3e39a84fa7cbf2d5b505
Joshua Shevitz <joshshev@yahoo.com>
2020-10-15 21:11:36 -0400

    database connected to server

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { connect } from "./backend/database/database";
+
+const app = express();
+const PORT = 8082;
+
+connect();
+
+app.get('/', (req,res) => res.send('Express + TypeScript Server'));
+app.listen(PORT, () => {
+  
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT} thank you very much`);
+
+});
