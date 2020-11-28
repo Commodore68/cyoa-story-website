aebeb3d201fe8f9223dd240a05a4b5295ce99bcf
Makafui Dzeze <juliedzeze@juliedzeze.local>
2020-11-28 01:02:16 -0500

    Created StoryOverview Page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -13,6 +13,7 @@ import Profile from './pages/UserProfile/Profile';
 import ProfileSettings from './pages/UserProfile/ProfileComponents/ProfileSettings';
 import SearchPage from './pages/SearchPage/SearchPage';
 import {ProgressSpinner} from "primereact/progressspinner";
+import StoryOverview from './pages/home/StoryOverview';
 
 const App = (): JSX.Element => {
     const { isLoading } = useAuth0();
@@ -33,6 +34,9 @@ const App = (): JSX.Element => {
                 </Route>
                 <Route path="/SearchPage">
                     <SearchPage />
+                </Route>
+                <Route path="/StoryOverview">
+                    <StoryOverview />
                 </Route>
                 <ProtectedRoute path="/Profile" component={Profile}/>
                 <ProtectedRoute path="/ProfileSettings" component={ProfileSettings}/>
