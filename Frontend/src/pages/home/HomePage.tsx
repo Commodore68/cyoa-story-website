25219508f21d47e9a876d46beb3f44a011f97106
Joshua Shevitz <joshshev@yahoo.com>
2020-12-01 14:32:46 -0500

    changed homepage back from conflicts

--- Frontend/src/pages/home/HomePage.tsx
+++ Frontend/src/pages/home/HomePage.tsx
@@ -1,11 +1,6 @@
 import React, {useEffect, useState} from 'react';
-import SampleStories from '../../common/MainToolbar/DataTable';
-import {Author} from "../../../../Backend/types/Author";
 import {findOrCreateAuthorWrapper} from "../../common/utils/requests";
 import {useAuth0} from "@auth0/auth0-react";
-
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
 
 export const HomePage = (): JSX.Element => {
     const {isAuthenticated, user} = useAuth0();
