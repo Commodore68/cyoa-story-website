8f4ea464a6c7ac6589f462728a4ecc27dabf7f0d
Joshua Shevitz <joshshev@yahoo.com>
2020-12-01 14:38:38 -0500

    homepage conflict changes

--- Frontend/src/pages/home/HomePage.tsx
+++ Frontend/src/pages/home/HomePage.tsx
@@ -1,20 +1,20 @@
 import React, {useEffect, useState} from 'react';
+
+import {Author} from "../../../../Backend/types/Author";
 import {findOrCreateAuthorWrapper} from "../../common/utils/requests";
 import {useAuth0} from "@auth0/auth0-react";
 
+//this useEffect will only happen on the first page load as there are no values it is dependent on
 export const HomePage = (): JSX.Element => {
     const {isAuthenticated, user} = useAuth0();
-
+    //this useEffect will only happen on the first page load as there are no values it is dependent on
     useEffect(() => {
         findOrCreateAuthorWrapper({isAuthenticated, user});
     }, []);
-    
-    return (
-        <>
-            <div>
-                <h1>HomePage</h1>
-            </div>
-        </>
-    );
 
+    return (
+        <div>
+            <h1>HomePage</h1>
+        </div>
+    );
 };
