c9110c65b5423b1f1bdfd03dc8ec97c341c28fcb
Joshua Shevitz <joshshev@yahoo.com>
2020-12-01 14:40:49 -0500

    fixing merge conflicts

--- Frontend/src/pages/home/HomePage.tsx
+++ Frontend/src/pages/home/HomePage.tsx
@@ -4,9 +4,9 @@ import {Author} from "../../../../Backend/types/Author";
 import {findOrCreateAuthorWrapper} from "../../common/utils/requests";
 import {useAuth0} from "@auth0/auth0-react";
 
-//this useEffect will only happen on the first page load as there are no values it is dependent on
 export const HomePage = (): JSX.Element => {
     const {isAuthenticated, user} = useAuth0();
+
     //this useEffect will only happen on the first page load as there are no values it is dependent on
     useEffect(() => {
         findOrCreateAuthorWrapper({isAuthenticated, user});
