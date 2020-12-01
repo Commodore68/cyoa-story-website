cf0b0663ed435f4fbc23baa3ecf916a1983b5154
Andrewpling <61208790+Andrewpling@users.noreply.github.com>
2020-11-30 20:45:13 -0500

    Just more progress

--- Frontend/src/pages/home/HomePage.tsx
+++ Frontend/src/pages/home/HomePage.tsx
@@ -3,13 +3,41 @@ import React, {useEffect, useState} from 'react';
 import {Author} from "../../../../Backend/types/Author";
 import {findOrCreateAuthorWrapper} from "../../common/utils/requests";
 import {useAuth0} from "@auth0/auth0-react";
-import {RecentlyUpdated} from "./components/RecentlyUpdated";
+import RecentlyUpdated from "./components/RecentlyUpdated";
+import { storyStore } from '../../stores';
+import { httpRequest } from '../../common/utils/axios';
 
-export const HomePage = (): JSX.Element => {
+var data = {
+    storyTitle: ["Title1", "Title2", "Title3"],
+    dateUpdated: [new Date(), new Date(), new Date()],
+    author: ["Spongebob Squarepants", "Patrick Star", "Squidward Tentacles"]
+}
+
+interface RecentlyUpdatedProps{
+    params:{
+        id:string
+    }
+}
+
+export const HomePage = (props: RecentlyUpdatedProps) => {
     const {isAuthenticated, user} = useAuth0();
 
     //this useEffect will only happen on the first page load as there are no values it is dependent on
     useEffect(() => {
+        async function fetchData(){
+            const response = await httpRequest({
+                method: 'POST',
+                endpoint: '/api/stories/',
+                data:{
+                    data:{
+                        id: props.params.id,
+                    },
+                    type: 'find-one'
+                }
+            });
+            storyStore.story = response.data;
+        }
+        void fetchData();
         findOrCreateAuthorWrapper({isAuthenticated, user});
     }, []);
 
@@ -23,7 +51,7 @@ export const HomePage = (): JSX.Element => {
             <div className="p-m-2" style={{bottom:"0%", position:"fixed", fontSize:"10.5pt"}}>
                 Created by: Only Drew Fleming, no one else
             </div>
-            <RecentlyUpdated />
+            <RecentlyUpdated stories={storyStore.story} />
         </div>
 
     );
--- Frontend/src/pages/home/components/RecentlyUpdated.tsx
+++ Frontend/src/pages/home/components/RecentlyUpdated.tsx
@@ -2,8 +2,17 @@ import React from "react";
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 
+import { Story } from '../../../../../Backend/types/Story';
 
-export const RecentlyUpdated = () =>{
+interface RecentlyUpdatedProps{
+    stories: Story,
+}
+
+function RecentlyUpdated(props:RecentlyUpdatedProps): JSX.Element{
+    const {stories} = props;
+
+
+
     return(
         <div className="p-d-flex">
             <div style={{width:"5%"}} />
@@ -11,14 +20,16 @@ export const RecentlyUpdated = () =>{
                 <div className="p-mt-2" style={{fontSize:"18pt"}}>
                     Recently Updated Stories:
                 </div>
+                <div>{stories.dateUpdated}</div>
                 {/*<div style={{height:"400px", overflow:"scroll"}}>
                     <div style={{fontSize:"150pt"}}>
                         something to test
                     </div>
                 </div> */}
-
             </div>
             <div style={{width:"5%"}}/>
         </div>
     );
 };
+
+export default RecentlyUpdated;
